1910d4ecf9e2bbe6afa35b2ec109f28e1987cd8e
jingliancui <ericcui@qq.com>
2024-12-30 18:04:33 +0800

    add sample app code

---
+++ src/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using GoogleMobileAds;
+
+namespace SampleApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+#if IOS
+		GADMobileAds.SharedInstance.StartWithCompletionHandler(null);
+
+		GAMRequest request=GAMRequest.Request() as GAMRequest;
+		GAMInterstitialAd.LoadWithAdManagerAdUnitID(
+			"/21775744923/example/interstitial",
+			request!,
+			(ad,error)=>
+			{
+				if(error!=null)
+				{
+					Debug.WriteLine("Detect Google Ads Error:"+error.LocalizedDescription);
+				}
+				else
+				{
+					ad.PresentFromRootViewController(null);
+				}
+			});
+#endif
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
